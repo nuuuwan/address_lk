99e9ed0518ddffead8ce379a39aa6ea6cc7a129b
nuuuwan <nuuuwan@gmail.com>
2023-05-24 16:36:32 +0530

    various

--- src/view/organisms/OSMView.js
+++ src/view/organisms/OSMView.js
@@ -1,10 +1,11 @@
 import {Component} from 'react';
-import {MapContainer, TileLayer, Rectangle, useMapEvent, Tooltip} from 'react-leaflet';
+import {MapContainer, TileLayer, Rectangle, useMapEvent, SVGOverlay} from 'react-leaflet';
 import './OSMView.css';
 
 const URL_FORMAT = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const ZOOM = 18;
-const LATLNG_COLOMBO = [6.917257469436852, 79.86479649172456];
+const ZOOM = 8;
+const [MIN_LAT, MIN_LNG, MAX_LAT, MAX_LNG] = [5, 79, 10, 82];
+const LATLNG_MID = [(MIN_LAT + MAX_LAT)/2, (MIN_LNG + MAX_LNG)/2];
 
 const QUANTUM = 36;
 
@@ -14,47 +15,55 @@ function logQ(x) {
 
 const K = Math.pow(QUANTUM, parseInt(logQ(10000)))
 
-const [MIN_LAT, MIN_LNG, MAX_LAT, MAX_LNG] = [5.9, 79.5,9.9, 81.9];
 
 
-function getAddressLabel(lat, lng, latLevel) {
-  const steps = -logQ(latLevel);
+
+function getAddressLabel(lat, lng, level) {
+  const [nLat, nLng ]= [lat- MIN_LAT + 1, lng - MIN_LNG + 1];
+  const steps = -logQ(level);
   const nChars = Math.ceil(steps) +1;
-  function format(x, offset) {
-    return (x*K).toString(QUANTUM).substring(0, nChars + offset).toUpperCase();
+  function format(x) {
+    return  (x*K).toString(QUANTUM).toUpperCase().substring(0, nChars);
   }
-  return `${format(lat,0)}-${format(lng,1)}`;
+  return `${format(nLat,0)}-${format(nLng,0)}`;
 }
 
 export default class OSMView extends Component {
   constructor(props) {
     super(props);
-    const [minLat, minLng, maxLat, maxLng] = [MIN_LAT, MIN_LNG, MAX_LAT, MAX_LNG];
-    this.state = {minLat, minLng, maxLat, maxLng}
+    const displayBounds = null;
+    this.state = {displayBounds}
   }
   renderRectangles() {
-    const {minLat, minLng, maxLat, maxLng} = this.state;
+    const {displayBounds} = this.state;
+    if (!displayBounds) {
+      return null;
+    }
+    console.log({displayBounds});
+    const [[minLat , minLng] , [maxLat , maxLng]]= displayBounds;
+
     const [latSpan, lngSpan] = [maxLat - minLat, maxLng - minLng];
     function getLevel(x) {
-      return Math.pow(QUANTUM, Math.round(logQ(x),0));
+      const K = Math.pow(QUANTUM, 0.2)
+      return Math.pow(QUANTUM, Math.floor(logQ(x * K)));
     }
-    const latLevel = getLevel(latSpan)
-    const lngLevel = getLevel(lngSpan)
-    const level = Math.min(latLevel, lngLevel);
 
+    const level = Math.min(getLevel(latSpan), getLevel(lngSpan));
+    console.info({level})
     let lat0 = Math.round(minLat / level) * level;
     let lng0 = Math.round(minLng / level) * level;
     
     let renderedRectangles = [];
     for (let lat = lat0 - level; lat <= maxLat + level; lat += level) {
       for (let lng = lng0-level; lng <= maxLng+level; lng += level) {
+        const key = `${lat}-${lng}-${level}`;
         const bounds =[
           [lat, lng],
-          [lat + latLevel, lng + level],
+          [lat + level, lng + level],
         ]
-        const [midLat, midLng] = [(lat + latLevel/2), (lng + level/2)];
+        const [midLat, midLng] = [(lat + level/2), (lng + level/2)];
         
-        if (midLat + level < MIN_LAT || midLat -level > MAX_LAT || midLng + level < MIN_LNG || midLng-level > MAX_LNG) {
+        if (midLat < MIN_LAT || midLat > MAX_LAT || midLng  < MIN_LNG || midLng > MAX_LNG) {
           continue;
         }
         const label = getAddressLabel(midLat, midLng, level);
@@ -63,11 +72,17 @@ export default class OSMView extends Component {
             bounds={bounds}
             color="red"
             weight={1}
+            key={"rect-" +key}
           >
-            <Tooltip direction="center">
-          {label}
-      </Tooltip>
+            
             </Rectangle>
+        );
+        renderedRectangles.push(
+<SVGOverlay attributes={{ fill: 'black'}} bounds={bounds} key={"svg-" +key}>
+      <text x="50%" y="50%" fontSize="12" textAnchor="middle">
+        {label}
+      </text>
+    </SVGOverlay>
         );
 
       }
@@ -78,14 +93,13 @@ export default class OSMView extends Component {
   }
 
   render() {
-    const position = LATLNG_COLOMBO;
+    const position = LATLNG_MID;
 
     const EventComponent = function() {
       const map = useMapEvent('move', () => {
         const bounds = map.getBounds();0
-        const  [minLat, minLng, maxLat, maxLng] = [bounds._southWest.lat, bounds._southWest.lng, bounds._northEast.lat, bounds._northEast.lng]; 
-       console.log(minLat, minLng, maxLat, maxLng);
-       this.setState({minLat, minLng, maxLat, maxLng});
+        const displayBounds = [[bounds._southWest.lat, bounds._southWest.lng], [bounds._northEast.lat, bounds._northEast.lng]]; 
+       this.setState({displayBounds});
       });
       return null
     }.bind(this);
