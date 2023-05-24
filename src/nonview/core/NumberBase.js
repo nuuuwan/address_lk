9f95993d9891d17155d17bd6f1ffd7ab26529a26
nuuuwan <nuuuwan@gmail.com>
2023-05-24 19:39:45 +0530

    Text symbols

--- src/nonview/core/NumberBase.js
+++ src/nonview/core/NumberBase.js
@@ -1,12 +1,17 @@
-import NUMBER_SYMBOLS from "./NUMBER_SYMBOLS";
+import { NUMBER_SYMBOLS } from "./NUMBER_SYMBOLS.js";
+const N_SYMBOLS = NUMBER_SYMBOLS.length;
+console.info({ N_SYMBOLS });
 export default class NumberBase {
-  static format(base, x, nChars) {
-    let basic = x.toString(base).toUpperCase().substring(0, nChars);
-    for (let i = 0; i < base; i++) {
-      const symbol = NUMBER_SYMBOLS[i] + " ";
-      const digit = i.toString(base).toUpperCase();
-      basic = basic.replaceAll(digit, symbol);
+  static format(x, base) {
+    let factors = [];
+    let r = x;
+    while (r > 0) {
+      factors.push(r % base);
+      r = Math.floor(r / base);
     }
-    return basic;
+    return factors
+      .reverse()
+      .map((f) => NUMBER_SYMBOLS[parseInt((f * N_SYMBOLS) / base)])
+      .join(" · ");
   }
 }
--- src/view/organisms/OSMView.js
+++ src/view/organisms/OSMView.js
@@ -2,7 +2,6 @@ import { Component } from "react";
 import {
   MapContainer,
   TileLayer,
-  Rectangle,
   useMapEvent,
   SVGOverlay,
 } from "react-leaflet";
@@ -10,117 +9,89 @@ import "./OSMView.css";
 import NumberBase from "../../nonview/core/NumberBase.js";
 
 const URL_FORMAT = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const ZOOM = 8;
+const ZOOM = 16;
 const [MIN_LAT, MIN_LNG, MAX_LAT, MAX_LNG] = [5, 79, 10, 82];
-const LATLNG_MID = [(MIN_LAT + MAX_LAT) / 2, (MIN_LNG + MAX_LNG) / 2];
+const LATLNG_LIPTON_CIRCUS = [6.917272788217442, 79.8647961518609];
 
-const QUANTUM = 36;
+function getLabel([lat, lng]) {
+  const [spanLat, spanLng] = [MAX_LAT - MIN_LAT, MAX_LNG - MIN_LNG];
+  const maxSpan = Math.max(spanLat, spanLng);
 
-function logQ(x) {
-  return Math.log(x) / Math.log(QUANTUM);
-}
+  const [pLat, pLng] = [(lat - MIN_LAT) / maxSpan, (lng - MIN_LNG) / maxSpan];
+  const [maxPLat, maxPLng] = [spanLat / maxSpan, spanLng / maxSpan];
 
-const K = Math.pow(QUANTUM, parseInt(logQ(10000)));
-
-function getAddressLabel(lat, lng, level) {
-  const [nLat, nLng] = [lat - MIN_LAT + 1, lng - MIN_LNG + 1];
-  const steps = -logQ(level);
-  const nChars = Math.ceil(steps) + 1;
-  function format(x) {
-    return NumberBase.format(QUANTUM, x * K, nChars);
+  if (!(0 < pLat && pLat < maxPLat && 0 < pLng && pLng < maxPLng)) {
+    return "";
   }
-  return `${format(nLat, 0)}-${format(nLng, 0)}`;
+
+  const BASE = 512;
+  const CHAR_COUNT = 3;
+  const QUANTUM2 = Math.pow(BASE, CHAR_COUNT);
+  const QUANTUM = Math.sqrt(QUANTUM2);
+  const DEGREES_PER_SYMBOL = maxSpan / QUANTUM;
+  console.info({ DEGREES_PER_SYMBOL });
+
+  const n = parseInt(parseInt(pLat * QUANTUM) * QUANTUM + pLng * QUANTUM);
+  const s = NumberBase.format(n, BASE);
+  return `${s}`;
 }
 
 export default class OSMView extends Component {
   constructor(props) {
     super(props);
     const displayBounds = null;
-    this.state = { displayBounds };
+    const displayCenter = LATLNG_LIPTON_CIRCUS;
+    this.state = { displayBounds, displayCenter };
   }
-  renderRectangles() {
-    const { displayBounds } = this.state;
+
+  renderCenter() {
+    const { displayCenter, displayBounds } = this.state;
+
     if (!displayBounds) {
       return null;
     }
-    console.log({ displayBounds });
-    const [[minLat, minLng], [maxLat, maxLng]] = displayBounds;
 
-    const [latSpan, lngSpan] = [maxLat - minLat, maxLng - minLng];
-    function getLevel(x) {
-      const K = Math.pow(QUANTUM, 0.2);
-      return Math.pow(QUANTUM, Math.floor(logQ(x * K)));
-    }
+    const label = getLabel(displayCenter);
+    const key = "center-" + displayCenter.join("-");
 
-    const level = Math.min(getLevel(latSpan), getLevel(lngSpan));
-    console.info({ level });
-    let lat0 = Math.round(minLat / level) * level;
-    let lng0 = Math.round(minLng / level) * level;
-
-    let renderedRectangles = [];
-    for (let lat = lat0 - level; lat <= maxLat + level; lat += level) {
-      for (let lng = lng0 - level; lng <= maxLng + level; lng += level) {
-        const key = `${lat}-${lng}-${level}`;
-        const bounds = [
-          [lat, lng],
-          [lat + level, lng + level],
-        ];
-        const [midLat, midLng] = [lat + level / 2, lng + level / 2];
-
-        if (
-          midLat < MIN_LAT ||
-          midLat > MAX_LAT ||
-          midLng < MIN_LNG ||
-          midLng > MAX_LNG
-        ) {
-          continue;
-        }
-        const label = getAddressLabel(midLat, midLng, level);
-        renderedRectangles.push(
-          <Rectangle
-            bounds={bounds}
-            color="red"
-            weight={1}
-            key={"rect-" + key}
-          ></Rectangle>
-        );
-        renderedRectangles.push(
-          <SVGOverlay
-            attributes={{ fill: "black" }}
-            bounds={bounds}
-            key={"svg-" + key}
-          >
-            <text x="50%" y="50%" fontSize="12" textAnchor="middle">
-              {label}
-            </text>
-          </SVGOverlay>
-        );
-      }
-    }
-
-    return renderedRectangles;
+    return (
+      <SVGOverlay key={key} bounds={displayBounds}>
+        <circle cx="50%" cy="50%" r="10" fill="red" />
+        <text
+          x="50%"
+          y="55%"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="black"
+          fontSize="24"
+          fontFamily="Fjalla One"
+        >
+          {label}
+        </text>
+      </SVGOverlay>
+    );
   }
 
   render() {
-    const position = LATLNG_MID;
-
+    const { displayCenter } = this.state;
     const EventComponent = function () {
       const map = useMapEvent("move", () => {
         const bounds = map.getBounds();
-        0;
         const displayBounds = [
           [bounds._southWest.lat, bounds._southWest.lng],
           [bounds._northEast.lat, bounds._northEast.lng],
         ];
-        this.setState({ displayBounds });
+        const center = map.getCenter();
+        const displayCenter = [center.lat, center.lng];
+        this.setState({ displayBounds, displayCenter });
       });
       return null;
     }.bind(this);
 
     return (
-      <MapContainer center={position} zoom={ZOOM} scrollWheelZoom={true}>
+      <MapContainer center={displayCenter} zoom={ZOOM} scrollWheelZoom={true}>
         <TileLayer url={URL_FORMAT} />
-        {this.renderRectangles()}
+        {this.renderCenter()}
         <EventComponent />
       </MapContainer>
     );
