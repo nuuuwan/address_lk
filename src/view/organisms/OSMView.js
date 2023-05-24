6c739abc679532831a053bb02753f00b7c3cbbd4
nuuuwan <nuuuwan@gmail.com>
2023-05-24 13:55:44 +0530

    renderRectangles basic

--- src/view/organisms/OSMView.js
+++ src/view/organisms/OSMView.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import {MapContainer, TileLayer} from 'react-leaflet';
+import {MapContainer, TileLayer, Rectangle} from 'react-leaflet';
 import './OSMView.css';
 
 const URL_FORMAT = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -7,6 +7,35 @@ const ZOOM = 18;
 const LATLNG_COLOMBO = [6.917257469436852, 79.86479649172456];
 
 export default class OSMView extends Component {
+  renderRectangles() {
+    const [minLat, minLng, maxLat, maxLng] = [6.91, 79.86, 6.92, 79.87];
+    const [latSpan, lngSpan] = [maxLat - minLat, maxLng - minLng];
+    const latLevel = Math.pow(10, Math.round(Math.log10(latSpan),0)-1);
+    const lngLevel = Math.pow(10,Math.round(Math.log10(lngSpan),0)-1);
+    
+    let lat0 = Math.round(minLat / latLevel) * latLevel;
+    let lng0 = Math.round(minLng / lngLevel) * lngLevel;
+    
+    let renderedRectangles = [];
+    for (let lat = lat0; lat < maxLat; lat += latLevel) {
+      for (let lng = lng0; lng < maxLng; lng += lngLevel) {
+        renderedRectangles.push(
+          <Rectangle
+            bounds={[
+              [lat, lng],
+              [lat + latLevel, lng + lngLevel],
+            ]}
+            color="red"
+            weight={1}
+          />
+        );
+      }
+    }
+    
+
+    return renderedRectangles;
+  }
+
   render() {
     const position = LATLNG_COLOMBO;
     return (
@@ -14,6 +43,7 @@ export default class OSMView extends Component {
         <TileLayer
           url={URL_FORMAT}
         />
+        {this.renderRectangles()}
       </MapContainer>
     );
   }
