ce88471931d9eb17a2fc26cc0f7a2fbf99ae7831
nuuuwan <nuuuwan@gmail.com>
2023-05-24 13:32:10 +0530

    use OSMView

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-
+import OSMView from './view/organisms/OSMView';
 function App() {
   return (
     <div className="App">
-      A new addressing system for Sri Lanka. 
+      A new addressing system for Sri Lanka.
+      <OSMView/>
     </div>
   );
 }
 
 export default App;
+
+
