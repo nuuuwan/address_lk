51dfe0fc14222065e1d0e765c4936a1a82e8e25a
nuuuwan <nuuuwan@gmail.com>
2023-05-25 10:59:02 +0530

    various

--- src/view/molecules/WordleGrid.js
+++ src/view/molecules/WordleGrid.js
@@ -2,6 +2,7 @@ import {
   CHAR_COUNT,
   BACKGROUND_COLORS,
   COLORS,
+  CHARS_PER_WORD,
 } from "../../nonview/core/constants.js";
 
 import WordleSquare from "../atoms/WordleSquare.js";
@@ -18,13 +19,15 @@ export default function WordleGrid({
   for (var j = 0; j < CHAR_COUNT; j++) {
     var i_color;
     var inner = [];
-    for (var i = 0; i < 5; i++) {
-      const charLoc = 5 * j + i;
+    for (var i = 0; i < CHARS_PER_WORD; i++) {
+      const charLoc = CHARS_PER_WORD * j + i;
       const c = displayLabel.charAt(charLoc);
       if (isDisplayLabelValid) {
-        if (c === displayLabel.charAt(5 * (CHAR_COUNT - 1) + i)) {
+        if (c === displayLabel.charAt(CHARS_PER_WORD * (CHAR_COUNT - 1) + i)) {
           i_color = 3;
-        } else if (displayLabel.substring(5 * (CHAR_COUNT - 1)).includes(c)) {
+        } else if (
+          displayLabel.substring(CHARS_PER_WORD * (CHAR_COUNT - 1)).includes(c)
+        ) {
           i_color = 2;
         } else {
           i_color = 1;
@@ -33,12 +36,28 @@ export default function WordleGrid({
         i_color = 0;
       }
 
-      const onChangeChar = function (char) {
-        const newLabel =
+      const onChangeChar = function (chars) {
+        if (chars === "BACKSPACE") {
+          onChangeLabel(
+            displayLabel,
+            (charLoc - 2 + CHAR_COUNT * CHARS_PER_WORD) %
+              (CHAR_COUNT * CHARS_PER_WORD)
+          );
+          return;
+        }
+
+        if (chars.length !== 1) {
+          return;
+        }
+
+        let newLabel =
           displayLabel.substring(0, charLoc) +
-          char +
+          chars +
           displayLabel.substring(charLoc + 1);
-        onChangeLabel(newLabel);
+        if (newLabel.length > CHAR_COUNT * CHARS_PER_WORD) {
+          newLabel = newLabel.substring(0, CHAR_COUNT * CHARS_PER_WORD);
+        }
+        onChangeLabel(newLabel, charLoc);
       };
 
       const backgroundColor = BACKGROUND_COLORS[i_color];
