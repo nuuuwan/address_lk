e08534f57d9730356f8f836a168f097acc05d3a8
nuuuwan <nuuuwan@gmail.com>
2023-05-25 09:43:06 +0530

    atom CanterTargetView

---
+++ src/view/atoms/CenterTargetView.js
@@ -0,0 +1,30 @@
+export default function CenterTargetView() {
+  return (
+    <svg className="svg-center" width="100%" height="100vh">
+      <circle
+        cx="50%"
+        cy="50%"
+        r="8%"
+        fill="red"
+        fillOpacity="0.5"
+        stroke="none"
+      />
+      <circle
+        cx="50%"
+        cy="50%"
+        r="5%"
+        fill="red"
+        fillOpacity="0.5"
+        stroke="none"
+      />
+      <circle
+        cx="50%"
+        cy="50%"
+        r="2%"
+        fill="red"
+        fillOpacity="0.5"
+        stroke="none"
+      />
+    </svg>
+  );
+}
