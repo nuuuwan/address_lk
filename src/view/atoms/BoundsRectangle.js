86c6f3cab259ba0f05dc865b1df02a95b6371bb4
nuuuwan <nuuuwan@gmail.com>
2023-05-25 17:46:39 +0530

    RADIUS = 1;

--- src/view/atoms/BoundsRectangle.js
+++ src/view/atoms/BoundsRectangle.js
@@ -2,6 +2,8 @@ import { Rectangle } from "react-leaflet";
 import { LATS_PER_QUANT, LNGS_PER_QUANT } from "../../nonview/core/constants";
 import LatLngToWord from "../../nonview/core/LatLngToWord";
 
+const RADIUS = 1;
+
 function SingleRectangle({ nLat0, nLng0, dlat, dlng }) {
   const nLat = nLat0 + dlat * LATS_PER_QUANT;
   const nLng = nLng0 + dlng * LNGS_PER_QUANT;
@@ -12,10 +14,10 @@ function SingleRectangle({ nLat0, nLng0, dlat, dlng }) {
   ];
 
   const isCenter = dlat == 0 && dlng == 0;
-  const color =isCenter ? "red" : "#f8f8f8";
-  const opacity =isCenter ? 1 : 0.5;
+  const color = isCenter ? "red" : "#f8f8f8";
+  const opacity = isCenter ? 1 : 0.5;
   const fillColor = "none";
-  const pathOptions = { fillColor, color, weight: 1 ,opacity};
+  const pathOptions = { fillColor, color, weight: 1, opacity };
 
   return <Rectangle bounds={rectBounds} pathOptions={pathOptions} />;
 }
@@ -23,11 +25,10 @@ function SingleRectangle({ nLat0, nLng0, dlat, dlng }) {
 export default function BoundsRectangle({ displayCenter }) {
   const [nLat0, nLng0] = LatLngToWord.normalizeLatLng(displayCenter);
 
-  const radius =10;
   let renderedRectangles = [];
 
-  for (let dlat = -radius; dlat <= radius; dlat++) {
-    for (let dlng = -radius; dlng <= radius; dlng++) {
+  for (let dlat = -RADIUS; dlat <= RADIUS; dlat++) {
+    for (let dlng = -RADIUS; dlng <= RADIUS; dlng++) {
       if (dlat == 0 && dlng == 0) continue;
 
       renderedRectangles.push(
